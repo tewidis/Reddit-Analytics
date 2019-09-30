5aa590ed64ba78ebd5e825fd17e51a364baaa632
Tanner Widis <tewidis@gmail.com>
2019-09-29 22:06:28 -0500

    Removing unnecessary code, writing some comments

--- code/reddit_scraper.py
+++ code/reddit_scraper.py
@@ -3,6 +3,7 @@ import pandas as pd
 import configparser
 
 def main():
+    # parse the configuration file to get the reddit credentials for the project. client_secret should remain secret, so moving it to a separate, untracked file
     cfg = configparser.ConfigParser();
     cfg.read('/home/twidis/discgolf_analytics/code/credentials.ini');
 
@@ -10,6 +11,7 @@ def main():
                         client_id=cfg.get('reddit_credentials', 'client_id'),
                         client_secret=cfg.get('reddit_credentials', 'client_secret'));
 
+    # get the id of each post to be analyzed
     post_ids = {'2013': '13wixp',
             '2014': '2fc0a9',
             '2015': '3hwlt4',
@@ -17,6 +19,7 @@ def main():
             '2018': '9bpmke',
             '2019': 'd9kcvu'};
 
+    # loop over the posts, doing a depth first search to get the brand, name, plastic, and run of each disc, then write to a csv
     for ids in post_ids:
         submission = reddit.submission(id=post_ids[ids]);
 
@@ -27,6 +30,7 @@ def main():
         df.to_csv('/home/twidis/discgolf_analytics/data/data' + ids + '.csv', index=False);
 
 def dfs_discs(list_of_discs):
+    # define a dict and loop over the comments to populate it with the required data
     comments_dict = { "brand":[], "disc":[], "plastic":[], "run":[], "score":[] };
 
     for brand in list_of_discs:
@@ -34,33 +38,18 @@ def dfs_discs(list_of_discs):
             for plastic in disc.replies:
                 if not len(plastic.replies) == 0:
                     for run in plastic.replies:
-                        #comments_dict["disc"].append(brand.body + ' ' + disc.body + ' ' + plastic.body + ' ' + run.body);
                         comments_dict["brand"].append(brand.body);
                         comments_dict["disc"].append(disc.body);
                         comments_dict["plastic"].append(plastic.body);
                         comments_dict["run"].append(run.body);
                         comments_dict["score"].append(run.score);
                 else:
-                    #comments_dict["disc"].append(brand.body + ' ' + disc.body + ' ' + plastic.body);
                     comments_dict["brand"].append(brand.body);
                     comments_dict["disc"].append(disc.body);
                     comments_dict["plastic"].append(plastic.body);
                     comments_dict["run"].append([]);
                     comments_dict["score"].append(plastic.score);
     return comments_dict;
-
-def depth_first_search(full_list):
-    #for top_level_comments in full_list:
-    queue = list(full_list);
-    print(full_list);
-    comments = [];
-
-    while queue:
-        comment = queue.pop(0);
-        comments.append(comment);
-        queue[0:0] = comment.replies;
-
-    return comments;
 
 if __name__ == "__main__":
     main();
